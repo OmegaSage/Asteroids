d0893ffd09c8bf92687ec3d21ed436deee788c2d
OmegaSage <his.righteousness@hotmail.com>
2025-12-20 00:18:59 -0500

    Player ship spins!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,20 +12,22 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0, 0, 0))
 
     clock = pygame.time.Clock()
     dt = 0
 
-    while True:
-        clock.tick(60)
-        dt = clock.get_time() / 1000.0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60) / 1000.0
+        screen.fill(("black"))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
 
